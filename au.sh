4a56a896c494de21c9716661a58ca2ca13eb6ff4
21d5 <99817034@qq.com>
2019-02-28 17:09:35 +0800

    Update au.sh

--- au.sh
+++ au.sh
@@ -4,15 +4,15 @@
 
 PATH=$(cd `dirname $0`; pwd)
 
-echo $PATH"/alydns.php"
+echo $PATH"/ali.py"
 
 # 调用 Python 脚本，自动设置 DNS TXT 记录。
 # 第一个参数：需要为那个域名设置 DNS 记录
 # 第二个参数：letsencrypt 动态传递的 Value 值 
 
-echo $CERTBOT_DOMAIN"_acme-challenge"$CERTBOT_VALIDATION
+echo $CERTBOT_DOMAIN $CERTBOT_VALIDATION
 
-$PATH"/alydns.php"  $CERTBOT_DOMAIN $CERTBOT_VALIDATION >"/var/log/certdebug.log"
+$PATH"/ali.py"  $CERTBOT_DOMAIN $CERTBOT_VALIDATION >"/var/log/certdebug.log"
 
 # DNS TXT 记录刷新时间
 /bin/sleep 20
